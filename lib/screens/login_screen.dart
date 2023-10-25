ff3b198acd80fb3c2b0eb24e6966f8443849b91c
Lotfi Gouasmi <gouasmimedlotfi@gmail.com>
2023-10-25 11:19:28 +0100

    started backend work

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  TextEditingController email=TextEditingController();
+  TextEditingController password=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
