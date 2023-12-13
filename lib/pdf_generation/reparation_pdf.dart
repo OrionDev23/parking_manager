2e44d0933369073df43453a37b62cae4399dbd6c
Lotfi Gouasmi <gouasmimedlotfi@gmail.com>
2023-12-13 15:40:08 +0100

    Done with reparation form, need to add errors testing table

---
+++ lib/pdf_generation/reparation_pdf.dart
@@ -0,0 +1,12 @@
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart';
+
+import '../serializables/reparation.dart';
+
+class ReparationPdf{
+  final Reparation reparation;
+
+  ReparationPdf({required this.reparation});
+
+
+}
