6de3b6751f97dd76a898f9bff62b89f626ac2d75
Lotfi Gouasmi <gouasmimedlotfi@gmail.com>
2024-01-14 10:50:33 +0100

    Finished vehicle dashboard

--- lib/screens/chauffeur/disponibilite/disponibilite_gestion.dart
+++ lib/screens/chauffeur/disponibilite/disponibilite_gestion.dart
@@ -1,9 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import '../../../../theme.dart';
 import '../../../../widgets/page_header.dart';
 import 'disponibilite_table.dart';
 
@@ -23,7 +20,6 @@ class DisponibiliteGestionState extends State<DisponibiliteGestion> {
 
   @override
   Widget build(BuildContext context) {
-    var appTheme=context.watch<AppTheme>();
     return ScaffoldPage(
       header: PageTitle(
         text: 'disponibilite'.tr(),
